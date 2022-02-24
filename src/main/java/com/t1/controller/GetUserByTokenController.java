02253316b126188cc98859078a5d862aca3f3c93
andywarhol <36893088+andywarhol@users.noreply.github.com>
2022-02-24 12:25:01 -0700

    Update GetUserByTokenController.java

--- src/main/java/com/t1/controller/GetUserByTokenController.java
+++ src/main/java/com/t1/controller/GetUserByTokenController.java
@@ -17,8 +17,8 @@ import com.t1.responsedto.ResponseDTO;
 import com.t1.responsedto.UserResponseDTO;
 
 
-//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
-@CrossOrigin("*", maxAge=3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//@CrossOrigin("*", maxAge=3600)
 @RestController
 @RequestMapping("/api/verify-token")
 public class GetUserByTokenController {
