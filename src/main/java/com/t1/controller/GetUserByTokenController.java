8b6af3e839e66d31a212124b7ca42e93fa9b783c
andywarhol <36893088+andywarhol@users.noreply.github.com>
2022-02-23 17:15:15 -0700

    Update GetUserByTokenController.java

--- src/main/java/com/t1/controller/GetUserByTokenController.java
+++ src/main/java/com/t1/controller/GetUserByTokenController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.t1.config.JWTUtility;
 import com.t1.entity.UserEntity;
 import com.t1.mapper.UserMapper;
 import com.t1.repository.UserDetailsRepository;
 import com.t1.responsedto.ResponseDTO;
 import com.t1.responsedto.UserResponseDTO;
+
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/verify-token")
