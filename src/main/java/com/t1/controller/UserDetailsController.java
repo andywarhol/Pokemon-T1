7300dd606ae2796eb4c86bc245aa3865c3272038
andywarhol <36893088+andywarhol@users.noreply.github.com>
2022-02-23 17:15:44 -0700

    Update UserDetailsController.java

--- src/main/java/com/t1/controller/UserDetailsController.java
+++ src/main/java/com/t1/controller/UserDetailsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.t1.config.JWTUtility;
 import com.t1.requestedto.LoginRequestDTO;
 import com.t1.responsedto.JWTResponseDTO;
