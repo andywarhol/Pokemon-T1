2957a3dd12be20aee98e566038c827e122030ca2
Andrea Mariana Urrea Urias <andrea.urrea@neoris.com>
2022-02-24 08:30:46 -0700

    Exceptions

--- src/main/java/com/t1/controller/GetUserByTokenController.java
+++ src/main/java/com/t1/controller/GetUserByTokenController.java
@@ -3,6 +3,7 @@ package com.t1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import com.t1.repository.UserDetailsRepository;
 import com.t1.responsedto.ResponseDTO;
 import com.t1.responsedto.UserResponseDTO;
 
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/verify-token")
 public class GetUserByTokenController {
--- src/main/java/com/t1/controller/UserDetailsController.java
+++ src/main/java/com/t1/controller/UserDetailsController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,9 @@ import com.t1.requestedto.LoginRequestDTO;
 import com.t1.responsedto.JWTResponseDTO;
 import com.t1.responsedto.ResponseDTO;
 
+
+//sustituir
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/api/authenticate/")
 public class UserDetailsController {
