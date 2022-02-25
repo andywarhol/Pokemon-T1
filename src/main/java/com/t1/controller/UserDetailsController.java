7996e9efb7c95125b2ec96e582d40782e4dad4b2
andywarhol <36893088+andywarhol@users.noreply.github.com>
2022-02-24 17:27:58 -0700

    Update UserDetailsController.java

--- src/main/java/com/t1/controller/UserDetailsController.java
+++ src/main/java/com/t1/controller/UserDetailsController.java
@@ -22,8 +22,8 @@ import com.t1.requestedto.LoginRequestDTO;
 import com.t1.responsedto.JWTResponseDTO;
 import com.t1.responsedto.ResponseDTO;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
-//@CrossOrigin("*", maxAge=3600)
+//@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/authenticate/")
 public class UserDetailsController {
